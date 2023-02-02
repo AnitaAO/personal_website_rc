ecbf468a880daefe144c0f450d5a3d260fb726fb
Amarachi Ajah <anitaisbeta@gmail.com>
2023-02-02 08:54:29 +0100

    update testimonial array

--- src/Components/Testimonial/Testimonial.jsx
+++ src/Components/Testimonial/Testimonial.jsx
@@ -2,8 +2,30 @@ import React from 'react';
 import './Testimonial.css'
 import {Swiper, SwiperSlide} from 'swiper/react';
 import 'swiper/css';
+import profilePics1 from '../../img/profile1.jpg';
+import profilePics2 from '../../img/profile2.jpg';
+import profilePics3 from '../../img/profile3.jpg';
+import profilePics4 from '../../img/profile4.jpg';
 
 const Testimonial = () => {
+  const clients = [
+    { img: profilePics1,
+      review: ''
+    },
+    {
+      img: profilePics2,
+      review: ''
+    },
+    {
+      img: profilePics3,
+      review: ''
+    },
+    {
+      img: profilePics4,
+      review: ''
+    }
+  ]
+
   return (
     <div className="t-wrapper">
 
@@ -18,10 +40,16 @@ const Testimonial = () => {
 
       {/* the slider */}
       <Swiper>
-        <SwiperSlide></SwiperSlide>
-        <SwiperSlide></SwiperSlide>
-        <SwiperSlide></SwiperSlide>
-        <SwiperSlide></SwiperSlide>
+        {clients.map((client, index) =>{
+          return(
+            <SwiperSlide
+              key={index}
+            >
+              <img src={client.img} alt='client' />
+              <span>{client.review}</span>
+            </SwiperSlide>
+          )
+        })}
       </Swiper>
     </div>
   )
